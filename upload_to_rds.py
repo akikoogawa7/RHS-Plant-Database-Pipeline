9cb83403e27ce72cb965efd58dc37a397b3edbda
Akiko Ogawa <akikoogawa@Akikos-MacBook-Pro.local>
2021-06-27 14:05:30 +0100

    Add RDS config file

---
+++ upload_to_rds.py
@@ -0,0 +1,26 @@
+# %%
+from sqlalchemy import create_engine 
+import pandas as pd
+
+#%%
+# Setting up the DB
+user = 'postgres'
+password = 'postgres'
+hostname = 'plants-db-instance.cp7nhwi8jhqz.us-east-2.rds.amazonaws.com'
+port = '5432'
+db_name = 'postgres'
+db_string = f"postgresql://{user}:{password}@{hostname}:{port}/{db_name}"
+
+#%%
+# Create engine
+db = create_engine(db_string)
+
+#%%
+# Open csv file
+with open('test_rhs_plant_database.csv') as f:
+    df = pd.read_csv(f)
+
+#%%
+# insert data to db
+df.to_sql('plant_database', db)
+# %%
